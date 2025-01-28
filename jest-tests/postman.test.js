6f13e7f23c42b92bf6c4900ba03791bfce88f3d8
lekhanagodugunuri <90751765+lekhanagodugunuri@users.noreply.github.com>
2025-01-28 10:34:42 +0100

    Fixed invalid URL issue by replacing Postman variables

--- jest-tests/postman.test.js
+++ jest-tests/postman.test.js
@@ -3,17 +3,27 @@ const path = require('path');
 const { Collection } = require('postman-collection');
 const axios = require('axios');
 
-
 const collectionPath = path.resolve(__dirname, '../Postman/Locality_324.postman_collection.json');
 const collectionJSON = JSON.parse(fs.readFileSync(collectionPath, 'utf-8'));
 const collection = new Collection(collectionJSON);
 
+const environmentVariables = {
+  base_url: process.env.BASE_URL || 'http://localhost:8080'
+};
+
+function replaceVariables(url) {
+  return url.replace(/{{(.*?)}}/g, (match, varName) => {
+    return environmentVariables[varName] || match;
+  });
+}
+
 describe('Postman API Tests', () => {
-  collection.items.each((item) => {  // ✅ FIX: Use .each() instead of .forEach()
+  collection.items.each((item) => {
     test(`${item.name}`, async () => {
       if (item.request) {
         const request = item.request;
-        const url = request.url.toString();
+        let url = request.url.toString();
+        url = replaceVariables(url);
         const method = request.method;
 
         const headers = {};
